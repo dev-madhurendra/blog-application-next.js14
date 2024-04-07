83b29771498c28f464954ee7737ab7bd19879055
dev-madhurendra <madhuredra.tiwari@zemosolabs.com>
2024-04-07 14:54:02 +0530

    feat : client server of blog application

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+export const MONGO_URI="mongodb+srv://user:blognextpassword@cluster0.crofx9n.mongodb.net/blog-next14?retryWrites=true&w=majority&appName=Cluster0"
+export const AUTH_SECRET="0FputhmPLaqo8mRxXozraI/e1g6mXtxmB2ZaDhQmgQA="
+export const AUTH_URL="http://localhost:3000/api/auth"
+export const GITHUB_ID = "ec086cf3f86fe42f0c30"
+export const GITHUB_SECRET = "a9f100ac59ad38bd6ecfbb5b2c6bf39a7c77ce86"
---
+++ src/lib/utils.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+import { MONGO_URI } from "./constants";
+
+const connection = {};
+
+export const connectToDb = async () => {
+  try {
+    if(connection.isConnected) {
+      console.log("Using existing connection");
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO_URI || MONGO_URI);
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+
